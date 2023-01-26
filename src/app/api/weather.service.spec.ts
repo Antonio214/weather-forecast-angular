b5ff8fd73d891aaa75de2e3b30ddcd5d7d1009f1
Antonio214 <antoniojuniorlk366@gmail.com>
2023-01-26 16:00:44 -0300

    added http tests

--- src/app/api/weather.service.spec.ts
+++ src/app/api/weather.service.spec.ts
@@ -1,14 +1,28 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
 import { TestBed } from '@angular/core/testing';
-import { example } from 'src/assets/mocks/exampleForecastCampinas';
-import { exampleSplitted } from 'src/assets/mocks/exampleSplittedByDay';
+import { HttpClient } from '@angular/common/http';
 
 import { WeatherService } from './weather.service';
+import { example } from 'src/assets/mocks/exampleForecastCampinas';
+import { apiKey, baseUrl } from './openweather-constants';
 
 describe('WeatherService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   let service: WeatherService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(WeatherService);
   });
 
@@ -17,18 +31,24 @@ describe('WeatherService', () => {
   });
 
   it('should return 5days forecast for a city', () => {
-    const data = service.getForecastFor('campinas');
+    const observable = service.getForecastFor('campinas');
+    observable.subscribe((data) => {
+      expect(data.success).toBeTrue();
+      expect(data.days.length).toBe(6);
+      expect(data.days[0].minTemperature).toBe('24.04');
+      expect(data.days[0].maxTemperature).toBe('29.89');
+      expect(data.days[0].averageTemperature).toBe('27.51');
+      expect(data.days[0].resume).toBe('Clouds');
+      expect(data.days[0].formattedDate).toBe('25 de Janeiro');
+      expect(data.days[0].weekDay).toBe('Quarta-Feira');
+      expect(data.days[0].description).toBe(
+        'Hoje vai estar nublado, mas não deixe isso estragar seu dia! Aproveite para fazer atividades indoor ou pegue um guarda-chuva para se divertir ao ar livre.'
+      );
+    });
 
-    expect(data.success).toBeTrue();
-    expect(data.days.length).toBe(6);
-    expect(data.days[0].minTemperature).toBe('24.04');
-    expect(data.days[0].maxTemperature).toBe('29.89');
-    expect(data.days[0].averageTemperature).toBe('27.51');
-    expect(data.days[0].resume).toBe('Clouds');
-    expect(data.days[0].formattedDate).toBe('25 de Janeiro');
-    expect(data.days[0].weekDay).toBe('Quarta-Feira');
-    expect(data.days[0].description).toBe(
-      'Hoje vai estar nublado, mas não deixe isso estragar seu dia! Aproveite para fazer atividades indoor ou pegue um guarda-chuva para se divertir ao ar livre.'
+    const req = httpTestingController.expectOne(
+      `${baseUrl}/data/2.5/forecast?lat=-22.90556&lon=-47.06083&appid=${apiKey}`
     );
+    req.flush(example);
   });
 });
